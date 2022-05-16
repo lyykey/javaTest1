c862916f7c8ed40458d4343c2f27f88d268490ad
DDldl2665 <104444386+DDldl2665@users.noreply.github.com>
2022-05-16 23:56:16 +0800

    0516新增註解版林盈利 練習Github

--- src/main/java/finalproject/finalprojecttest/DataHolder.java
+++ src/main/java/finalproject/finalprojecttest/DataHolder.java
@@ -4,18 +4,40 @@ import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 
 public class DataHolder {
-//    ImageView ve = new ImageView();
+    ImageView ve = new ImageView();
     private static DataHolder dataHolder = new DataHolder();
     private static DataHolder dataHolder2 = new DataHolder();
-
     private DataHolder(){}
 
     public static DataHolder get(){
         return dataHolder;
     }
 
-    public int pre_pos;
     public int pos;
+    public int selectIndex;
+    /**
+     * 當前是哪位玩家在擲骰子
+     * 1為玩家一 2為玩家二 以此類推
+     * */
+    public static int currentPlayer = 1;
+    /**
+     * 當前是哪位玩家發生移動 1為玩家一 2為玩家二 以此類推*/
+    private static int whoMove = 0;
+    /**
+     * 公用值static 整數 移動玩家所移動的步數*/
+    private static int moveSteps = 0;
+    /**
+     * 用來設定 whoMove 的方法*/
+    public void setWhoMove(int who) { whoMove = who; }
+    /**
+     * 用來設定 moveSteps 的方法*/
+    public void setMoveSteps(int steps) { moveSteps = steps; }
+    /**
+     * @return moveSteps*/
+    public int getMoveSteps() { return moveSteps; }
+    /**
+     * @return whoMove*/
+    public int getWhoMove() { return whoMove; }
 
 
 
@@ -23,9 +45,20 @@ public class DataHolder {
         Image c = v.getImage();
         return c;
     }
-    /*
-    林盈利
-    */
+    /**
+     * getDataHolder1 是用來獲取dataHolder
+     * @author 林盈利
+     * */
+    public DataHolder getDataHolder1(){
+        return dataHolder;
+    }
+    /**
+     * getDataHolder2 是用來獲取dataHolder2
+     * @author 林盈利
+     * */
+    public DataHolder getDataHolder2(){
+        return dataHolder2;
+    }
     public boolean changePos(int value){
         if(pos + value > 100 || pos +value < 0)
             return false;
@@ -35,7 +68,7 @@ public class DataHolder {
     }
     /*
 
-    */
+     */
     public void setImage(String name,ImageView v){
         Image a = new Image(name);
         v.setImage(a);
--- src/main/java/finalproject/finalprojecttest/Dice.java
+++ src/main/java/finalproject/finalprojecttest/Dice.java
@@ -1,17 +1,60 @@
 package finalproject.finalprojecttest;
 
 public class Dice {
+    public DataHolder data = DataHolder.get();
+    public int diceValueForAction=0; //骰出來的值
+    public int diceValueForSteps = 0;
+    public int currentPlayerInt = 1;
+    public String[] playerName = {"玩家一","玩家二","玩家三","玩家四"};
+
 
     public Dice(){
         /*
          可改為每次建構就RANDOM隨機數字
          */
+
         this.value =1;
     }
     int value;
-
-    public int reDice(){
-        this.value = 2;
-        return value;
+    /**
+     * 設定的玩家少於需要的玩家會出現錯誤
+     * @param movePlayer 要移動的玩家
+     * @param moveSteps 移動步數 往前為正整數 往後為負整數
+     * @author 林盈利
+     * */
+    public void changePlayerPosition(int movePlayer, int moveSteps){
+        data.setWhoMove(movePlayer);
+        data.setMoveSteps(moveSteps);
+        DataHolder movePlayerData ;
+        try {
+            switch (movePlayer) {
+                case 1 -> movePlayerData = data.getDataHolder1();
+                case 2 -> movePlayerData = data.getDataHolder2();
+                default -> throw new Exception();
+            }
+        }catch (Exception ex) {
+            System.out.println("movePlayerData未初始化 在Dice.java changePlayerPosition");
+            movePlayerData = data.getDataHolder1();
+            movePlayerData.pos -= moveSteps;
+        }
+        movePlayerData.pos += moveSteps;
     }
+    /**
+     * 用來擲動作 步數
+     * */
+    public void rollDice(){
+        diceValueForAction= (int)(Math.random()*4); // 骰作何種動作
+        diceValueForSteps = (int)(Math.random()*4); // 骰走幾步
+    }
+    /**
+     * 用來設定現在是誰在骰*/
+    public void whoRolling(int whichPlayer){
+        this.currentPlayerInt = whichPlayer;
+        switch (whichPlayer) { //用來設定現在是誰在骰骰子
+            case 1 -> currentPlayerInt = 1;
+            case 2 -> currentPlayerInt = 2;
+        }
+    }
+
+
 }
