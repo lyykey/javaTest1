ebb18cc3a27fa69559cbeb4f60f2494f3ae2b64b
DDldl2665 <104444386+DDldl2665@users.noreply.github.com>
2022-06-03 15:29:26 +0800

    新增 檢查勝負

--- src/main/java/finalproject/finalprojecttest/Game2Controller.java
+++ src/main/java/finalproject/finalprojecttest/Game2Controller.java
@@ -50,6 +50,7 @@ public class Game2Controller implements EventHandler<KeyEvent> {
     private Timeline animationLeft;
     private Timeline animationRight;
     private Timeline animationTime;
+    private Timeline animationCheck;
     private double rightSpeed = 1, leftSpeed = 1;
     private Image imageNum0 = new Image("數字零.png");
     private Image imageNum1 = new Image("數字一.png");
@@ -60,15 +61,9 @@ public class Game2Controller implements EventHandler<KeyEvent> {
     @FXML
     public void startButtonOnPressed(){
         hintPane.setVisible(false);
-        initialBall();
-        animationLeft = new Timeline(new KeyFrame(Duration.millis(20), e -> moveLeft()));
-        animationLeft.setCycleCount(Timeline.INDEFINITE);
-        animationLeft.play();
-        animationRight = new Timeline(new KeyFrame(Duration.millis(20), e -> moveRight()));
-        animationRight.setCycleCount(Timeline.INDEFINITE);
-        animationRight.play();
         Controller3.game2Scene.getRoot().requestFocus();
-        showCountdownPane();
+        startGame();
+
     }
     private void initialBall(){
         rightSpeed = 1;
@@ -82,21 +77,95 @@ public class Game2Controller implements EventHandler<KeyEvent> {
         leftRock.setStyle(null);
         leftScissors.setStyle(null);
         timeCounter.setProgress(0);
-        timeImage.setImage(imageNum0);
+        timeImage.setImage(imageNum3);
+        timeImage.setVisible(true);
+        RPFirstType = true;
+        LPFirsType = true;
+        firstType = true;
+        ballArrive = false;
+        leftPlayerChoice = 0;
+        rightPlayerChoice = 0;
     }
-    private  void showCountdownPane(){
+    private void startGame(){
+        System.out.println("startGame function");
+        initialBall();
+        animationLeft = new Timeline(new KeyFrame(Duration.millis(20), e -> moveLeft()));
+        animationLeft.setCycleCount(Timeline.INDEFINITE);
+        animationRight = new Timeline(new KeyFrame(Duration.millis(20), e -> moveRight()));
+        animationRight.setCycleCount(Timeline.INDEFINITE);
+        animationCheck = new Timeline(new KeyFrame(Duration.millis(20), e -> checkWhoWin()));
+        animationCheck.setCycleCount(Timeline.INDEFINITE);
+
+        showCountdownPane();
+        animationCheck.play();
+
+
+    }
+    private int whoWin = 0, round = 0;
+    private void tie(){
+        if(round < 3){
+            round++;
+            startGame();
+        }else whoWin = (int)(Math.random()*2);
+    }
+    private void checkWhoWin(){
+        if(ballArrive) {
+            if(RPFirstType && !LPFirsType) whoWin = 2;
+            if(!RPFirstType && LPFirsType) whoWin = 1;
+            if(RPFirstType && LPFirsType) {
+                tie();
+            }
+        }else {
+            if(!RPFirstType && !LPFirsType) {
+                switch (rightPlayerChoice) {
+                    case 1 -> {
+                        switch (leftPlayerChoice) {
+                            case 1 -> tie();
+                            case 2 -> whoWin = 1;
+                            case 3 -> whoWin = 2;
+                        }
+                    }
+                    case 2 -> {
+                        switch (leftPlayerChoice) {
+                            case 1 -> whoWin = 2;
+                            case 2 -> tie();
+                            case 3 -> whoWin = 1;
+                        }
+                    }
+                    case 3 -> {
+                        switch (leftPlayerChoice) {
+                            case 1 -> whoWin = 1;
+                            case 2 -> whoWin = 2;
+                            case 3 -> tie();
+                        }
+                    }
+                }
+            }
+        }
+        if(whoWin != 0) {
+            animationCheck.pause();
+            System.out.println(whoWin);
+        }
+    }
+    private void showCountdownPane(){
         animationTime = new Timeline(new KeyFrame(Duration.millis(20), e -> moveTime()));
+        animationTime.setCycleCount(Timeline.INDEFINITE);
         animationTime.play();
     }
     private  void moveTime(){
-        timeCounter.setProgress(timeCounter.getProgress()+0.01);
-        if(timeCounter.getProgress() == 0.33) timeImage.setImage(imageNum2);
-        if(timeCounter.getProgress() == 0.66) timeImage.setImage(imageNum1);
-        if(timeCounter.getProgress() == 1) {
+        timeCounter.setProgress(timeCounter.getProgress()+0.005);
+        if(timeCounter.getProgress() >= 0.33) timeImage.setImage(imageNum2);
+        if(timeCounter.getProgress() >= 0.66) timeImage.setImage(imageNum1);
+        if(timeCounter.getProgress() >= 0.99) {
             timeImage.setImage(imageNum0);
             animationTime.stop();
+            timeImage.setVisible(false);
+            animationLeft.play();
+            animationRight.play();
         }
+
     }
+    boolean RPFirstType = true, LPFirsType = true, firstType = true, ballArrive = false;
     private void moveLeft(){
         Circle circle = circleLeft;
         double x = circle.getCenterX();
@@ -107,6 +176,7 @@ public class Game2Controller implements EventHandler<KeyEvent> {
         else{
             x = 720;
             animationLeft.pause();
+            ballArrive = true;
         }
         circle.setCenterX(x);
     }
@@ -120,28 +190,85 @@ public class Game2Controller implements EventHandler<KeyEvent> {
         else{
             x = 780;
             animationRight.pause();
+            ballArrive = true;
         }
         circle.setCenterX(x);
     }
 
 
-
+    private int leftPlayerChoice = 0, rightPlayerChoice = 0;
 
     @Override
     public void handle(KeyEvent keyEvent) {
         KeyCode e = keyEvent.getCode();
         System.out.println(e);
-        switch (e) {
-            case NUMPAD1 -> {
-                rightScissors.setStyle("-fx-background-color: blue");
+        if(!ballArrive){
+            if(e == KeyCode.NUMPAD1 || e == KeyCode.NUMPAD2 || e == KeyCode.NUMPAD3) {
+                if(RPFirstType) {
+                    switch (e) {
+                        case NUMPAD1 -> rightPlayerChoice = 1;
+                        case NUMPAD2 -> rightPlayerChoice = 2;
+                        case NUMPAD3 -> rightPlayerChoice = 3;
+                    }
+                    animationRight.pause();
+                    RPFirstType = false;
+                    switch (rightPlayerChoice) {
+                        case 1 -> rightScissors.setStyle("-fx-background-color: blue");
+                        case 2 -> rightRock.setStyle("-fx-background-color: blue");
+                        case 3 -> rightPaper.setStyle("-fx-background-color: blue");
+                    }
+                    if(firstType) {
+                        firstType = false;
+                        switch (rightPlayerChoice) {
+                            case 1 -> leftRock.setStyle("-fx-background-color: red");
+                            case 2 -> leftPaper.setStyle("-fx-background-color: red");
+                            case 3 -> leftScissors.setStyle("-fx-background-color: red");
+                        }
+                    }
 
+                }
             }
-            case NUMPAD2 -> {
-                System.out.println(rightScissors.getBackground());
-                if(!String.valueOf(rightScissors.getBackground()).equals("null")){
-                    System.out.println("有東西");
-                }else System.out.println("沒東西");
+            else if(e == KeyCode.J || e == KeyCode.K || e == KeyCode.L) {
+                if(LPFirsType) {
+                    animationLeft.pause();
+                    LPFirsType = false;
+                    switch (e) {
+                        case J -> {
+                            leftPlayerChoice = 1;
+                            leftScissors.setStyle("-fx-background-color: blue");
+                        }
+                        case K -> {
+                            leftPlayerChoice = 2;
+                            leftRock.setStyle("-fx-background-color: blue");
+                        }
+                        case L -> {
+                            leftPlayerChoice = 3;
+                            leftPaper.setStyle("-fx-background-color: blue");
+                        }
+                    }
+                    if(firstType) {
+                        firstType = false;
+                        switch (leftPlayerChoice) {
+                            case 1 -> rightRock.setStyle("-fx-background-color: red");
+                            case 2 -> rightPaper.setStyle("-fx-background-color: red");
+                            case 3 -> rightScissors.setStyle("-fx-background-color: red");
+                        }
+                    }
+                }
             }
         }
+
+//        switch (e) {
+//            case NUMPAD1 -> {
+//                rightScissors.setStyle("-fx-background-color: blue");
+//
+//            }
+//            case NUMPAD2 -> {
+//                System.out.println(rightScissors.getBackground());
+//                if(!String.valueOf(rightScissors.getBackground()).equals("null")){
+//                    System.out.println("有東西");
+//                }else System.out.println("沒東西");
+//            }
+//        }
     }
 }
